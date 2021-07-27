30f80e130efce02fa1f47542a631ef57f5bacd14
Anna Vu <anna.h.vu9@gmail.com>
2021-07-26 21:54:57 -0500

    prepare

--- prepare.py
+++ prepare.py
@@ -1,12 +1,11 @@
 import unicodedata
 import re
 import json
-
 import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 from nltk.corpus import stopwords
-
 import pandas as pd
+
 
 def basic_clean(stringcheese):
     '''
@@ -26,6 +25,7 @@ def basic_clean(stringcheese):
     #return our basic clean string
     return stringcheese
 
+
 def tokenize(stringcheese):
     '''
     tokenize will take in a string and tokenize all of the words in it
@@ -42,14 +42,89 @@ def stem(stringcheese):
     '''
     accepts text and returns it after stemming all of the words
     '''
+    #create the stem
     ps = nltk.porter.PorterStemmer()
+    
+    #stem the words
     stems = [ps.stem(word) for word in stringcheese.split()]
+    
+    #combine the stems, separate the stems with spaces
+    stems = ' '.join(stems)
+    
+    #return the stems
     return stems
+
 
 def lemmatize(stringcheese):
     '''
     takes in a string and lemmatizes it
     '''
+    #initialize the lemmatizer
     wnl = nltk.stem.WordNetLemmatizer()
+    
+    #lemmatize the suspects
     lemmas = [wnl.lemmatize(word) for word in stringcheese.split()]
+    
+    #combine the lemmas, separate the lemmas with spaces
+    lemmas = ' '.join(lemmas)
+    
+    #return the lemmas
     return lemmas
+
+
+def remove_stopwords(words):
+    '''
+    takes in some text and removes stop words
+    '''
+    #initialize the initial stopword list
+    stopword_list = stopwords.words('english')
+    
+    #take out the words we want to exclude from it (the words we want to KEEP)
+    stopword_list = set(stopword_list) - set(exclude_words)
+    #add the words we do want to our stop word list (words we do NOT want)
+    stopword_list = stopword_list.union(set(extra_words))
+    
+    #split our words
+    words = words.split()
+    
+    #only keep words that are not in the stopwords list
+    filtered_words = [word for word in words if word not in stopword_list]
+    #separate our filtered words with spaces
+    article_without_stopwords = ' '.join(filtered_words)
+    
+    #return our article with no stop words
+    return article_without_stopwords
+
+
+def prep_article_data(df, content, extra_words=[], exclude_words=[]):
+    '''
+    prep_article_data is like the final step in the figurative sandwich making process,
+    it will take in a dataframe, the content column as a string, words to add to the stop word list,
+    and words you want to take out of the stop word list.
+    
+    Then it will return the title, the original content, a clean content, stemmed content, and lemmatized content
+    into a nice dataframe figuratively toasted to perfection.
+    '''
+    #title
+    df['title'] = df.title
+    
+    #original content
+    df['original']= df.content   
+    df['clean'] = df[content].apply(basic_clean)\
+                    .apply(tokenize)\
+                    .apply(remove_stopwords)
+    
+    #stemmed
+    df['stemmed']= df[content].apply(basic_clean)\
+                    .apply(tokenize)\
+                    .apply(stem)\
+                    .apply(remove_stopwords, extra_words = extra_words, exclude_words = exclude_words)
+    
+    #lemmatized
+    df['lemmatized'] = df[content].apply(basic_clean)\
+                    .apply(tokenize)\
+                    .apply(lemmatize)\
+                    .apply(remove_stopwords, extra_words = extra_words, exclude_words = exclude_words)
+    
+    #put it all together
+    return df[['title', 'original','clean','stemmed','lemmatized']] 
